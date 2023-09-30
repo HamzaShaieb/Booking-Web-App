95d9ec5f413d404d367fab8991d4b87db11d2532
HamzaShaieb <hamzashaiebdev@gmail.com>
2023-09-30 16:00:21 +0100

    Last Update

--- client/src/index.js
+++ client/src/index.js
@@ -5,14 +5,11 @@ import './index.css';
 import { AuthContextProvider } from './context/AuthContext';
 import { SearchContextProvider } from './context/SearchContext';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById('root')); // Replace 'root' with your actual root element ID
 root.render(
-  <React.StrictMode>
-    <AuthContextProvider>
-      <SearchContextProvider>
-        <App />
-      </SearchContextProvider>
-    </AuthContextProvider>
-  </React.StrictMode>
-);
+
+  <App/>
+
+ );
+
 
